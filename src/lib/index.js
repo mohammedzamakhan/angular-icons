4ab8a8e802e821cc82dbbd0e762ae90a07aa4e65
Zama Khan Mohammed <mohammedzamakhan@gmail.com>
2017-05-27 13:10:54 -0500

    build(UnwantedFiles): remove unwanted files (#2)

--- src/lib/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var IconBase_component_1 = require("./src/component/IconBase.component");
-exports.IconBaseComponent = IconBase_component_1.IconBaseComponent;
-var _500px_1 = require("./src/component/fa/500px");
-exports.Fa500pxComponent = _500px_1.Fa500pxComponent;
-var module_1 = require("./src/module");
-exports.IconsModule = module_1.IconsModule;
-//# sourceMappingURL=index.js.map
